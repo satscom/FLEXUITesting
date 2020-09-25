1faab5ccf6408adfb3848855929ce469aea52ccd
Sathish <satscom@gmail.com>
2020-09-25 17:06:27 -0400

    Added Before Hook

--- cypress/integration/Home.test.js
+++ cypress/integration/Home.test.js
@@ -1,8 +1,16 @@
+
 describe('Login Page', () => {
+
+    beforeEach(() => {
+
+        console.log("Before hook");
+
+    });
+
 
     it('should display header', () => {
           
-        cy.visit("https://glass.acptfanniemae.com/portal#/signin");
+       // Add cy.visit here
         cy.contains("Welcome");
         cy.get('input[name=email]').type('test');
         cy.get('input[name=password]').type(`test{enter}`);
