8635fc21bcc9fce8b19ea4fa08cd43c52ccb7827
Sathish <satscom@gmail.com>
2020-09-25 16:37:44 -0400

    Added invalid login check

--- cypress/integration/Home.test.js
+++ cypress/integration/Home.test.js
@@ -4,6 +4,10 @@ describe('Login Page', () => {
           
         cy.visit("https://glass.acptfanniemae.com/portal#/signin");
         cy.contains("Welcome");
+        cy.get('input[name=email]').type('test');
+        cy.get('input[name=password]').type(`test{enter}`);
+        cy.contains("We didn't recognize your credentials. Please try again");
+    
         
     })
 
