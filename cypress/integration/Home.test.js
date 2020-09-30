96dbc2c5bfed5011c96150a929f663c87a976b53
Kevin Uruena <kru8xr@virginia.edu>
2020-09-30 16:59:08 -0400

    Adding logout for Management and FR screen

--- cypress/integration/Home.test.js
+++ cypress/integration/Home.test.js
@@ -1,16 +1,13 @@
+// const { it } = require("mocha");
 
 describe('Login Page', () => {
 
     beforeEach(() => {
-
         cy.fixture('asapplus.json').as("asapJSON");
-
         cy.server();
-
         cy.route('/fez/flexapi/asapmanagement/summary/ALL/**', "@asapJSON").as("asap");
       //  cy.route('POST','/cdxapi/gettoken',{})
       //  cy.route('POST','/cdxapi/isValidSession',{})
-
     });
 
 
@@ -22,6 +19,13 @@ describe('Login Page', () => {
         cy.get('input[name=password]').type(Cypress.env("password"));
         cy.contains('SIGN IN').click();
         cy.contains('Early Funding').click();
+    })
+
+    it('user logs out', () => {
+        cy.get('.ui-dd-menu-item-icon').click()
+        // cy.get('span[class=ui-menuitem-text]').should('include', 'Logout')
+        cy.get('span[class=ui-menuitem-text]').click()
+        cy.url().should('include', '/signin')
     })
 
     
